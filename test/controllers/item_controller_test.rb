047e60ef5aa3110bd36c0513a41815dedf5f49d2
utsugi <urjn12345@gmail.com>
2017-02-04 18:54:48 +0900

    ver3

--- test/controllers/item_controller_test.rb
+++ test/controllers/item_controller_test.rb
@@ -6,14 +6,4 @@ class ItemControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
 end
